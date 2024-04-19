bb04e8cca3d72843664bfda7d70b7d6a84889c18
Lemois 1337 <lemois1337@protonmail.com>
2024-04-19 15:34:18 +0200

    Using karlsend v1.2.0 and Go module updates

--- processing/main.go
+++ processing/main.go
@@ -23,7 +23,7 @@ func main() {
 	}
 
 	logging.Logger().Infof("Application version %s", versionPackage.Version())
-	logging.Logger().Infof("Embedded kalrsend version %s", version.Version())
+	logging.Logger().Infof("Embedded karlsend version %s", version.Version())
 	logging.Logger().Infof("Network %s", config.ActiveNetParams.Name)
 
 	database, err := databasePackage.Connect(config.DatabaseConnectionString)
